ec80afacbfdf6c7802a5fae2fcf6bb86c3859cb9
andela-bomotoso <bukola.omotoso@andela.com>
2016-01-10 13:13:33 +0100

    Get current user activity and add more string values

--- MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/MainActivity.java
+++ MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/MainActivity.java
@@ -1,8 +1,11 @@
 package com.andela.omotoso.bukola.movementtracker;
 
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -11,8 +14,18 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.LocationListener;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.location.LocationListener;
+
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -33,6 +46,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -42,14 +56,17 @@ LocationListener,ResultCallback<Status> {
     private Context context;
     private Button trackerButton;
     private GoogleApiClient googleApiClient;
+    private GoogleApiClient googleApiClientActivity;
     private LocationRequest locationRequest;
     private TextView longLatText;
     private TextView currentLocationText;
     private double longitude = 0;
     private double latitude = 0;
+    private TextView currentActivityText;
     private Geocoder geocoder;
     private String street;
     protected ActivityDetectionBroadcastReceiver activityDetectionBroadcastReceiver;
+    private final String TAG = "LOCATION_FINDER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +74,6 @@ LocationListener,ResultCallback<Status> {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,6 +83,8 @@ LocationListener,ResultCallback<Status> {
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        buildGoogleApiClient();
+        activityDetectionBroadcastReceiver = new ActivityDetectionBroadcastReceiver();
         geocoder = new Geocoder(MainActivity.this);
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
@@ -74,17 +92,20 @@ LocationListener,ResultCallback<Status> {
                 .addOnConnectionFailedListener(this).build();
         longLatText = (TextView)findViewById(R.id.current_longlatText);
         currentLocationText = (TextView)findViewById(R.id.current_locationText);
+        currentActivityText = (TextView)findViewById(R.id.current_ActivityText);
         context = getApplicationContext();
         trackerButton = (ToggleButton)findViewById(R.id.tracker_button);
-        trackerButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        trackerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 boolean on = ((ToggleButton) view).isChecked();
-                if(on) {
-                    Toast.makeText(context,"Tracking Started",Toast.LENGTH_SHORT).show();
+                if (on) {
+                    Toast.makeText(context, "Tracking Started", Toast.LENGTH_SHORT).show();
+                    startTracking();
                 } else
-                    Toast.makeText(context,"Tracking Stopped", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "Tracking Stopped", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
     @Override
@@ -148,12 +169,26 @@ LocationListener,ResultCallback<Status> {
     protected void onStart() {
         super.onStart();
         googleApiClient.connect();
+        googleApiClientActivity.connect();
     }
 
     @Override
     protected void onStop() {
         googleApiClient.disconnect();
+        googleApiClientActivity.disconnect();
         super.onStop();
+    }
+
+    @Override
+    protected void onPause()  {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(activityDetectionBroadcastReceiver);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        LocalBroadcastManager.getInstance(this).registerReceiver(activityDetectionBroadcastReceiver, new IntentFilter(Constants.BROADCAST_ACTION));
+        super.onResume();
     }
 
     @Override
@@ -161,7 +196,10 @@ LocationListener,ResultCallback<Status> {
         locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         locationRequest.setInterval(1000);
-        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+        if(googleApiClient.isConnected()) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+            }
     }
 
     @Override
@@ -195,6 +233,14 @@ LocationListener,ResultCallback<Status> {
             return street;
     }
 
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClientActivity = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(ActivityRecognition.API)
+                .build();
+    }
+
     public void onResult(Status status) {
         if(status.isSuccess()) {
             Log.e(TAG, "Successfully added activity detection");
@@ -203,30 +249,87 @@ LocationListener,ResultCallback<Status> {
     }
 
     private PendingIntent getActivityDetectionPendingIntent() {
-        Intent intent = new Intent(this,DetailedActivitiesIntentService.class);
+        Intent intent = new Intent(this,ActivityDetector.class);
         return PendingIntent.getService(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
     }
-    public void requestActivityUpdate(View view) {
-        if(!googleApiClient.isConnected()) {
-            Toast.makeText(this,getString(R.string.error_msg),Toast.LENGTH_SHORT).show();
-            return;
+    public void startTracking() {
+//        if(!googleApiClientActivity.isConnected()) {
+//            Toast.makeText(this,getString(R.string.error_msg),Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+        if(googleApiClientActivity.isConnected()) {
+            ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(googleApiClientActivity, Constants.DETECTION_INTERVAL_IN_MILLISECONDS,
+                    getActivityDetectionPendingIntent()).setResultCallback(this);
         }
-        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(googleApiClient,Constants.DETECTION_INTERVAL_IN_MILLISECONDS,
-                getActivityDetectionPendingIntent()).setResultCallback(this);
-        requestUpdates.setEnabled(false);
-        removeUpdates.setEnabled(true);
     }
 
-    public void removeActivityUpdate(View view) {
-        if(!googleApiClient.isConnected()) {
-            Toast.makeText(this,getString(R.string.error_msg),Toast.LENGTH_SHORT).show();
-            return;
+    public void stopTracking() {
+
+//        ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(googleApiClient,getActivityDetectionPendingIntent())
+//                .setResultCallback(this);
+
+        //statusTextView.setText("");
+    }
+
+    public class ActivityDetectionBroadcastReceiver extends BroadcastReceiver {
+        protected static final String TAG = "receiver";
+
+        public void onReceive(Context context, Intent intent) {
+            List<Integer>confidenceLevels = new ArrayList<>();
+            List<String>activities = new ArrayList<>();
+            ArrayList<DetectedActivity> updatedActivities = intent.getParcelableArrayListExtra(Constants.ACTIVITY_EXTRA);
+            String status = "";
+            for(DetectedActivity activity : updatedActivities) {
+
+                status += getActivityType(activity.getType()) + activity.getConfidence()+"%\n";
+                activities.add(activity.getType()+"");
+                confidenceLevels.add(activity.getConfidence());
+            }
+            Log.d("Detected Activity", status);
+//            Log.d("activity size",activities.size()+"");
+//            Log.d("confidence size",confidenceLevels.size()+"");
+//            Log.d("current activity",getHighestActivityConfidence(confidenceLevels,activities));
+            currentActivityText.setText(getHighestActivityConfidence(confidenceLevels, activities));
+            //statusTextView.setText(status);
         }
-        ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(googleApiClient,getActivityDetectionPendingIntent())
-                .setResultCallback(this);
-        removeUpdates.setEnabled(false);
-        requestUpdates.setEnabled(true);
-        statusTextView.setText("");
+
+        public String getActivityType(int detectedActivityType) {
+            Resources resources = MainActivity.this.getResources();
+            switch (detectedActivityType){
+                case DetectedActivity.IN_VEHICLE:
+                    return resources.getString(R.string.on_vehicle);
+                case DetectedActivity.ON_BICYCLE:
+                    return resources.getString(R.string.on_bicycle);
+                case DetectedActivity.ON_FOOT:
+                    return resources.getString(R.string.on_foot);
+                case DetectedActivity.RUNNING:
+                    return resources.getString(R.string.running);
+                case DetectedActivity.STILL:
+                    return resources.getString(R.string.standing_still);
+                case DetectedActivity.TILTING:
+                    return resources.getString(R.string.tilting);
+                case DetectedActivity.UNKNOWN:
+                    return resources.getString(R.string.unknown);
+                case DetectedActivity.WALKING:
+                    return resources.getString(R.string.walking);
+                default:
+                    return resources.getString(R.string.unidentifiable);
+            }
+        }
+    }
+
+    public String getHighestActivityConfidence(List<Integer>confidenceLevels,List<String>activities) {
+        double max = confidenceLevels.get(0);
+        int count = 0;
+        String activity = "";
+        for(Integer value:confidenceLevels){
+            if(value > max){
+                max = value;
+                activity = activities.get(count);
+                count++;
+            }
+        }
+        return activity;
     }
 
 }
