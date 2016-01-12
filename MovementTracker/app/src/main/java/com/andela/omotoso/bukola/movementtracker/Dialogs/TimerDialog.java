ed4150875b1c9f8719cc8856244b75a1d68564b5
andela-bomotoso <bukola.omotoso@andela.com>
2016-01-12 13:41:42 +0100

    Add a layout to manage settings shared preferences

--- MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/Dialogs/TimerDialog.java
+++ MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/Dialogs/TimerDialog.java
@@ -1,7 +1,17 @@
 package com.andela.omotoso.bukola.movementtracker.Dialogs;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
 /**
  * Created by GRACE on 1/12/2016.
  */
-public class TimerDialog {
+public class TimerDialog extends DialogFragment {
+
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//    }
 }
