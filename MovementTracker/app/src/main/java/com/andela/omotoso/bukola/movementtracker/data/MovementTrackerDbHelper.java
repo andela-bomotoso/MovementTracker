225f533d97ada3906d30628ca64b4bf77653eaf9
andela-bomotoso <bukola.omotoso@andela.com>
2016-01-15 00:46:07 +0100

    Add a DbHelper class to manage database manipulations

---
+++ MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/data/MovementTrackerDbHelper.java
@@ -0,0 +1,10 @@
+package com.andela.omotoso.bukola.movementtracker.data;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by GRACE on 1/15/2016.
+ */
+public class MovementTrackerDbHelper extends SQLiteOpenHelper {
+    
+}
