9858fe7344da4ca0d0e22a58ee60ecb5cebcf965
andela-bomotoso <bukola.omotoso@andela.com>
2016-01-20 00:22:58 +0100

    Refactor the Date Picker fragment , the DatePickerListener andTimer Listener

--- MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/Utilities/TimerListener.java
+++ MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/Utilities/TimerListener.java
@@ -4,5 +4,6 @@ package com.andela.omotoso.bukola.movementtracker.utilities;
  * Created by GRACE on 1/19/2016.
  */
 public interface TimerListener {
+
     void onTick(String timeSpent);
 }
--- MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/date_picker/DatePickerFragment.java
+++ MovementTracker/app/src/main/java/com/andela/omotoso/bukola/movementtracker/date_picker/DatePickerFragment.java
@@ -15,6 +15,7 @@ import java.util.Calendar;
  * A simple {@link Fragment} subclass.
  */
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
     private Calendar calendar;
     private DatePickerListener listener;
     private String selectedDate;
@@ -22,39 +23,28 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 
 
     public DatePickerFragment() {
-        // Required empty public constructor
+
         dateHandler = new DateHandler();
     }
 
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        TextView textView = new TextView(getActivity());
-//        textView.setText(R.string.hello_blank_fragment);
-//        return textView;
-//    }
-
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker
+
         calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        // Do something with the date chosen by the user
-        //selectedDate = day+"-"+month+1+"-"+year;
+
         selectedDate = year+"-"+month+1+"-"+day;
         listener.onDatePicked(dateHandler.convertShortDateToLongDate(selectedDate));
-
     }
     public void setDatePickerListener(DatePickerListener listener) {
+
         this.listener = listener;
     }
 
