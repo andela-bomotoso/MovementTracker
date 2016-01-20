ee7131d6efd7315eb214608368d34b016e13ffe1
andela-bomotoso <bukola.omotoso@andela.com>
2016-01-20 21:01:37 +0100

    [Starts #112084155] Add a DateHandler test class to test the DateHandler class methods

---
+++ MovementTracker/app/src/androidTest/java/com/andela/omotoso/bukola/movementtracker/Utilities/DateHandlerTest.java
@@ -0,0 +1,34 @@
+package com.andela.omotoso.bukola.movementtracker.utilities;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by GRACE on 1/20/2016.
+ */
+public class DateHandlerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testGetCurrentDate() throws Exception {
+
+    }
+
+    public void testGetCurrentTime() throws Exception {
+
+    }
+
+    public void testFormatDate() throws Exception {
+
+    }
+
+    public void testConvertLongDateToShortDate() throws Exception {
+
+    }
+
+    public void testConvertShortDateToLongDate() throws Exception {
+
+    }
+}
